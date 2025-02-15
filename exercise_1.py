d57879977ad8d113e109a4e35727d8a4f1647302
MyoThet <mr.myothet@gmail.com>
2025-02-15 22:25:17 +0700

    exercise 1 and 2

---
+++ exercise_1.py
@@ -0,0 +1,3 @@
+print("Hours in a year", 365 * 24)
+print("Minutes in a decade", 10 * 365 * 24 * 60)
+print("seconds old", 38 * 365 * 24 * 60 * 60)
---
+++ exercise_2.py
@@ -0,0 +1,3 @@
+age_in_seconds = 1406 * (10**6)
+age_in_year = age_in_seconds / (365 * 24 * 60 * 60)
+print("My age in years is", int(round(age_in_year, 0)))
